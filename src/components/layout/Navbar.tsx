2a7376b980a1bdba202791c1348ae1330e97ab9b
Qian Gong <gongqian.de@gmail.com>
2026-04-29 12:29:53 +0200

    feat(navbar): add navigation between search and gallery views

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -2,13 +2,25 @@ import SearchBar from '@/components/ui/SearchBar';
 
 type NavbarProps = {
   onSearch: (query: string) => void;
+  onShowSearch: () => void;
+  onShowGallery: () => void;
 };
 
-const Navbar = ({ onSearch }: NavbarProps) => {
+const Navbar = ({ onSearch, onShowSearch, onShowGallery }: NavbarProps) => {
   return (
     <div className="navbar bg-base-100 shadow-sm">
-      <div className="flex-1">
-        <a className="btn btn-ghost text-xl">Art Gallery</a>
+      <div className="flex-1 gap-2">
+        <button
+          type="button"
+          className="btn btn-ghost text-xl"
+          onClick={onShowSearch}
+        >
+          Art Gallery
+        </button>
+
+        <button type="button" className="btn btn-ghost" onClick={onShowGallery}>
+          My Gallery
+        </button>
       </div>
 
       <SearchBar onSearch={onSearch} />
